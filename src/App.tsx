29e6f0cc07ef32b1c740d13cf6994034516a9cd5
jerry <jerrynwosu007@gmail.com>
2023-03-10 01:50:48 +0100

    auth with google and persist

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useEffect } from 'react';
+import { useAppDispatch } from './app/hooks';
 import GoogleButton from './components/GoogleButton';
+import { setUserLoggedIn } from './features/auth/authSlice';
 
 function App() {
+  const dispatch = useAppDispatch();
+  const auth = getAuth();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(
+          setUserLoggedIn({
+            uid: user.uid,
+            email: user.email,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          })
+        );
+      } else {
+        dispatch(setUserLoggedIn(null));
+      }
+    });
+
+    return () => unsubscribe();
+  }, [dispatch]);
+
   return (
     <div className="App">
       <GoogleButton />
--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,17 +1,26 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { auth } from '../../config/firebase';
-import { GoogleAuthProvider, signInWithPopup, User } from 'firebase/auth';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+
+interface User {
+  uid: string;
+  displayName: string | null;
+  email: string | null;
+  photoURL: string | null;
+}
 
 interface AuthState {
   user: null | User;
   status: 'idle' | 'pending' | 'fulfilled' | 'rejected';
   error: null | string;
+  isLoggedIn: boolean;
 }
 
 const initialState: AuthState = {
   user: null,
   status: 'idle',
   error: null,
+  isLoggedIn: false,
 };
 
 export const signInWithGoogle = createAsyncThunk(
@@ -20,8 +29,12 @@ export const signInWithGoogle = createAsyncThunk(
     try {
       const provider = new GoogleAuthProvider();
       const result = await signInWithPopup(auth, provider);
-      console.log(result);
-      return result.user;
+      return {
+        uid: result.user.uid,
+        displayName: result.user.displayName,
+        email: result.user.email,
+        photoURL: result.user.photoURL,
+      };
     } catch (error: any) {
       return rejectWithValue(error.message);
     }
@@ -31,7 +44,12 @@ export const signInWithGoogle = createAsyncThunk(
 const authSlice = createSlice({
   name: 'auth',
   initialState,
-  reducers: {},
+  reducers: {
+    setUserLoggedIn(state, action: PayloadAction<User | null>) {
+      state.user = action.payload;
+      state.isLoggedIn = true;
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(signInWithGoogle.pending, (state) => {
       state.status = 'pending';
@@ -53,4 +71,5 @@ const authSlice = createSlice({
   },
 });
 
+export const { setUserLoggedIn } = authSlice.actions;
 export default authSlice.reducer;
